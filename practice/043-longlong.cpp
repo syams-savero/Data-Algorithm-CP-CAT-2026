e1ff9cd21d5a65711e98d92669610450048c1a88
syams-savero <muhammadsyamssavero@gmail.com>
2026-04-07 21:03:38 +0700

    long long

---
+++ practice/043-longlong.cpp
@@ -0,0 +1,14 @@
+// tipe data int maksimal nya adalah -2pangkat31 hingga 2pangkat31 - 1
+// tipe data long long berada dalam rentan -2pangkat63 hingga 2pangkat63 - 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    long long a = 2002419288;
+    long long b = 1040323716;
+
+    long long luas = a * b;
+    cout << luas << endl;
+}
