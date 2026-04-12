e7de8a7b00d86a18df27b6a44d3c2e2c53e2b289
syams-savero <muhammadsyamssavero@gmail.com>
2026-04-12 23:16:51 +0700

    rename and rewrite into c++

---
+++ preparation/input-output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int c;
+    cin >> c;
+    cout << c << endl;
+}
+
+// tinggal di sesuaikan dengan kebutuhan variabel
---
+++ preparation/typedarray.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3] = {1, 2, 3};
+    cout << arr << endl;
+}
